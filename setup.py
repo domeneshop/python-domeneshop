e46c8f6de47aa1448bfcf7187ef2410ddfe836cd
Eirik Rye <rye@domeneshop.no>
2024-05-02 11:14:34 +0200

    Bump 0.4.4, remove dependencies of black/mypy/prospector (replaced by ruff)

--- setup.py
+++ setup.py
@@ -1,35 +1,35 @@
 from setuptools import find_packages, setup
 
-
 INSTALL_REQUIRES = ["urllib3", "certifi"]
 
-DEV_EXTRAS = ["black", "mypy", "prospector", "wheel"]
+DEV_EXTRAS = ["wheel"]
 
 with open("README.rst", "rb") as f:
     LONG_DESCRIPTION = f.read().decode("utf-8")
 
 setup(
     name="domeneshop",
-    version="0.4.3",
+    version="0.4.4",
     description="Domeneshop API library",
     author="Domeneshop AS",
     url="https://github.com/domeneshop/python-domeneshop",
     license="MIT License",
     long_description=LONG_DESCRIPTION,
-    python_requires="!=2.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires="!=2.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     classifiers=[
         "Environment :: Plugins",
         "Intended Audience :: System Administrators",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Security",
         "Topic :: System :: Installation/Setup",
